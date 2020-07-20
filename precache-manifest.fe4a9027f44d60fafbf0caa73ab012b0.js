fd3d4c950b4a820a3c6d400c0218cd735644268b
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-07-20 23:22:51 +0530

    Updates

---
+++ precache-manifest.fe4a9027f44d60fafbf0caa73ab012b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae259d5a20f3c9e35fd09cd83e9787b",
+    "url": "/drum-machine-react/index.html"
+  },
+  {
+    "revision": "b9429d365099fc63eca7",
+    "url": "/drum-machine-react/static/css/main.63622253.chunk.css"
+  },
+  {
+    "revision": "3782e8e9d13d7fb7fe02",
+    "url": "/drum-machine-react/static/js/2.0278cbae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine-react/static/js/2.0278cbae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9429d365099fc63eca7",
+    "url": "/drum-machine-react/static/js/main.361cb269.chunk.js"
+  },
+  {
+    "revision": "dc46ba9dcfd5dd555f91",
+    "url": "/drum-machine-react/static/js/runtime-main.5d7d986c.js"
+  }
+]);
